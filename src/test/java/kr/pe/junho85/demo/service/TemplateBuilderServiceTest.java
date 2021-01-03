a20591268dd7e0c4f64d4d994e7ad04bdddf5299
june.kim <junho85@gmail.com>
2021-01-03 17:38:47 +0900

    test

--- src/test/java/kr/pe/junho85/demo/service/TemplateBuilderServiceTest.java
+++ src/test/java/kr/pe/junho85/demo/service/TemplateBuilderServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -40,6 +42,75 @@ class TemplateBuilderServiceTest {
         // Then
         then(result)
                 .isEqualTo("Hello june");
+    }
+
+    @Test
+    void test3() {
+        // Given
+        final String templateString = "Hello ${user!}";
+        Map<String, String> model = Map.of();
+
+        // When
+        String result = templateBuilderService.build(templateString, model);
+
+        // Then
+        then(result)
+                .isEqualTo("Hello ");
+    }
+
+    @Test
+    void test4() {
+        // Given
+        final String templateString = "Hello ${user!\"member\"}";
+        Map<String, String> model = Map.of();
+
+        // When
+        String result = templateBuilderService.build(templateString, model);
+
+        // Then
+        then(result)
+                .isEqualTo("Hello member");
+    }
+
+    @Test
+    void test_date() {
+        // Given
+        final String templateString = "지금은 ${date?string('yyyy-MM-dd a hh:mm:ss')}";
+        Map<String, Object> model = Map.of("date", new Date());
+
+        // When
+        String result = templateBuilderService.build(templateString, model);
+
+        // Then
+        System.out.println(result); // e.g. 지금은 2021-01-03 오후 05:01:56
+    }
+
+    @Test
+    void test_date_not_exists() {
+        // Given
+        final String templateString = "지금은 ${(date?string('yyyy-MM-dd a hh:mm:ss'))!}";
+        Map<String, Object> model = Map.of();
+
+        // When
+        String result = templateBuilderService.build(templateString, model);
+
+        // Then
+        System.out.println(result); // e.g. 지금은
+    }
+
+    @Test
+    void test_date_null() {
+        // Given
+        final String templateString = "지금은 ${(date?string('yyyy-MM-dd a hh:mm:ss'))!}";
+        Map<String, Object> model = new HashMap<>() {{
+            put("date", null);
+        }};
+
+        // When
+        String result = templateBuilderService.build(templateString, model);
+
+        // Then
+        System.out.println(result); // e.g. 지금은
     }
 
 }
